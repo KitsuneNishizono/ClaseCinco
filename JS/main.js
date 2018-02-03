b3c004986d79c1cf70ee3747857b332b5852885f
Kitsune Nishizono <rrangel@hub-camp.mx>
2018-02-03 11:18:54 -0600

    sufrir con números erroneos

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+alert("Hola Mundo");
+var costo=100;
+console.log(costo);
+var cliente="Rocío Rangel";
+console.log("Rocío Rangel");
+var facturado;
+var iva = (1.16 * costo);
+console.log(Math.ceil(iva));
