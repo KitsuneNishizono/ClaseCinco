ec07a252fd2ab80389198eb3cf7f9eb49f62d133
Kitsune Nishizono <rrangel@hub-camp.mx>
2018-02-03 11:58:58 -0600

    Haciendo operaciones básicas matemáticas

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,9 @@
-alert("Hola Mundo");
-var costo=100;
+//alert("Hola Mundo");
+var person=prompt ("Escribe tu nombre");
+var costo=prompt ("¿De qué cantidad necesitas calcular el iva?");
 console.log(costo);
 var cliente="Rocío Rangel";
 console.log("Rocío Rangel");
 var facturado;
 var iva = (1.16 * costo);
-console.log(Math.ceil(iva));
+alert("Hola " + person + ", tú total a pagar con iva es de " + Math.ceil(iva));
